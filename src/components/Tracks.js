546a5d4bd7ea2d9c92a9659e5702eea5b33df369
Vinh Nguyen <ngthanhvinh2000@gmail.com>
2020-05-11 16:15:30 +0700

    Get all saved tracks; Display tracks in a simple way

--- src/components/Tracks.js
+++ src/components/Tracks.js
@@ -7,8 +7,41 @@ class Tracks extends Component {
 		this.props.fetchSavedTracks();
 	}
 
+	displayTrack = (item) => {
+		return (
+			<div style={{ border: '1px solid black', margin: '5px', padding: '5px' }}>
+				<a href={item.track.external_urls.spotify}>
+					<img src={item.track.album.images[1].url} alt={item.track.name}></img>
+				</a>
+				<p>Added at: {item.added_at}</p>
+				<p>Name: {item.track.name}</p>
+				<p>
+					{item.track.artists
+						.map((artist, key) => (
+							<a key={key} href={artist.external_urls.spotify}>
+								{artist.name}
+							</a>
+						))
+						.reduce((prev, curr) => [prev, ', ', curr])}
+				</p>
+			</div>
+		);
+	};
+
 	render() {
-		return <div>{JSON.stringify(this.props.tracks)}</div>;
+		const tracks = this.props.tracks;
+		console.log('tracks', tracks);
+		return tracks != null ? (
+			<div>
+				<ul>
+					{tracks.map((item, key) => {
+						return <li key={key}>{this.displayTrack(item)}</li>;
+					})}
+				</ul>
+			</div>
+		) : (
+			<div></div>
+		);
 	}
 }
 
--- src/services/api.js
+++ src/services/api.js
@@ -1,9 +1,9 @@
 import SpotifyWebApi from 'spotify-web-api-js';
 
 const spotifyApi = new SpotifyWebApi();
-console.log('nani');
 
 const api = {
+	// Get information of the current user
 	getUser: async (accessToken) => {
 		spotifyApi.setAccessToken(accessToken);
 		try {
@@ -14,13 +14,25 @@ const api = {
 		}
 	},
 
+	// Get all saved tracks of the current user
 	getSavedTracks: async (accessToken) => {
+		const limit = 50;
+		let offset = 0;
 		spotifyApi.setAccessToken(accessToken);
-		try {
-			const res = await spotifyApi.getMySavedTracks();
-			return res;
-		} catch (error) {
-			console.log(error);
+		let tracks = [];
+		while (true) {
+			try {
+				const res = await spotifyApi.getMySavedTracks({ limit, offset });
+				tracks = tracks.concat(res.items);
+				if (res.next == null) {
+					return tracks;
+				} else {
+					offset += limit;
+				}
+			} catch (error) {
+				console.log(error);
+				return;
+			}
 		}
 	},
 };
