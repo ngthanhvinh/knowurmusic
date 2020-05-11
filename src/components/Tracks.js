e4cf80a02ab0d7b3e1af6cec6ee6945b363b7c16
Duong Dac <19305859+eddrda@users.noreply.github.com>
2020-05-12 05:02:12 +0700

    Show tracks by year
    Added track showing & styling

--- src/components/Tracks.js
+++ src/components/Tracks.js
@@ -7,41 +7,64 @@ class Tracks extends Component {
 		this.props.fetchSavedTracks();
 	}
 
+	displayYear = (item) => { }
+
 	displayTrack = (item) => {
 		return (
-			<div style={{ border: '1px solid black', margin: '5px', padding: '5px' }}>
-				<a href={item.track.external_urls.spotify}>
-					<img src={item.track.album.images[1].url} alt={item.track.name}></img>
-				</a>
-				<p>Added at: {item.added_at}</p>
-				<p>Name: {item.track.name}</p>
-				<p>
-					{item.track.artists
-						.map((artist, key) => (
-							<a key={key} href={artist.external_urls.spotify}>
-								{artist.name}
-							</a>
-						))
-						.reduce((prev, curr) => [prev, ', ', curr])}
-				</p>
-			</div>
+			<div className="track">
+				<img className="album_cover" src={item.track.album.images[1].url} alt={item.track.name}></img>
+
+				<div className="track_info">
+					<div className="half">
+						<a href={item.track.external_urls.spotify}>
+							<p style={{ fontWeight: 800 }}>{item.track.name}</p>
+						</a></div>
+					<div className="half">
+						<p>
+							by {item.track.artists
+								.map((artist, key) => (
+									<a key={key} href={artist.external_urls.spotify}>
+										{artist.name}
+									</a>
+								))
+								.reduce((prev, curr) => [prev, ', ', curr])}
+						</p>
+					</div>
+				</div>
+			</div >
 		);
 	};
 
 	render() {
 		const tracks = this.props.tracks;
-		console.log('tracks', tracks);
-		return tracks != null ? (
-			<div>
-				<ul>
-					{tracks.map((item, key) => {
-						return <li key={key}>{this.displayTrack(item)}</li>;
-					})}
-				</ul>
-			</div>
-		) : (
-			<div></div>
-		);
+		if (tracks == null)
+			return null
+
+		var by_year = {};
+		console.log(tracks[0])
+		for (const key in tracks) {
+			const year = tracks[key].track.album.release_date.slice(0, 4)
+			if (!(year in by_year))
+				by_year[year] = []
+			by_year[year].push(tracks[key])
+		}
+
+		var years = []
+		for (const year in by_year) {
+			years.push(
+				<div className="year_outer" key={year}>
+					<h2 className="year_title">
+						<span style={{ color: "royalblue" }}>{year}</span>
+						<span> / {by_year[year].length} songs</span>
+					</h2>
+					<div className="year_inner">
+						{by_year[year].map((item, key) => {
+							return <span key={key}>{this.displayTrack(item)}</span>;
+						})}
+					</div>
+				</div>)
+		}
+		return years
 	}
 }
 
